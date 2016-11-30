5f65817801bf6681d0ba08bbe33158c640533865
jtlong3rd <jtlong3rd@gmail.com>
2016-11-29 19:29:01 -0500

    Refactor Exercise 7 solution to use ES6 constructs

--- exercise7.js
+++ exercise7.js
@@ -3,15 +3,11 @@
 // PROMPT: Write a route that extracts data from query string in the GET '/search' URL
 // route, e.g. ?results=recent&include_tabs=true and then outputs it back to
 // the user in JSON format.
-      
-var express = require('express');
-var app = express();
-var port = process.argv[2];
 
-app.get('/search', 
-        function(request, response) {
-            response.send(request.query);
-        }
-       );
-       
+const express = require('express');
+const app = express();
+const port = process.argv[2];
+
+app.get('/search', (request, response) => response.send(request.query));
+
 app.listen(port);
