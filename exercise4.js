8156309ef862615701751d685edbc2923daa2532
jtlong3rd <jtlong3rd@gmail.com>
2016-11-29 19:20:02 -0500

    Refactor Exercise 4 solution to use ES6 features

--- exercise4.js
+++ exercise4.js
@@ -3,21 +3,20 @@
 // PROMPT: Write a route ('/form') that processes HTML form input
 // (<form><input name="str"/></form>) and prints backwards the str value.
 
-var express = require('express');
-var app = express();
-var bodyparser = require('body-parser');
-var port = process.argv[2];
+const express = require('express');
+const app = express();
+const bodyparser = require('body-parser');
+const port = process.argv[2];
 
-app.post('/form', 
-        function(request, response) {
-            var urlencodedParser = bodyparser.urlencoded({extended: false});
-            urlencodedParser(request, response, function(err) {if (err) 
-                                                                throw err;
-                                                                
-                                                               response.send(request.body.str.split('').reverse().join(''));
-                                                              }
-                            );
-        }
-       );
-       
+app.post('/form', (request, response) => {
+  var urlencodedParser = bodyparser.urlencoded({ extended: false });
+  urlencodedParser(request, response, err => {
+    if (err) {
+      throw err;
+    }
+
+    response.send(request.body.str.split('').reverse().join(''));
+  });
+});
+
 app.listen(port);
