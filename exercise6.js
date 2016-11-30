e45786ed7085d68877f4ba7397484f30d5dd037d
jtlong3rd <jtlong3rd@gmail.com>
2016-11-29 19:27:34 -0500

    Refactor Exercise 6 to use ES6 constructs

--- exercise6.js
+++ exercise6.js
@@ -13,18 +13,16 @@
 //       .createHash('sha1')
 //       .update(new Date().toDateString() + id)
 //       .digest('hex')
-      
-var express = require('express');
-var app = express();
-var port = process.argv[2];
 
-app.put('/message/:id', 
-        function(request, response) {
-            response.send(require('crypto')
-                    .createHash('sha1')
-                    .update(new Date().toDateString() + request.params.id)
-                    .digest('hex'));
-        }
-       );
-       
+const express = require('express');
+const app = express();
+const port = process.argv[2];
+
+app.put('/message/:id', (request, response) => {
+  response.send(require('crypto')
+    .createHash('sha1')
+    .update(new Date().toDateString() + request.params.id)
+    .digest('hex'));
+});
+
 app.listen(port);
